d278107f8280895180ff2426240993e9efeff52a
Nilavan Anbumozhi <nilavanrajamani@gmail.com>
2019-07-31 18:06:20 +0530

    Add project files.

---
+++ WebAppTagHelperPrj/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WebAppTagHelperPrj.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
