5e70f2ff0945ed61bcb82269b009ea835338539a
Nilavan Anbumozhi <nilavanrajamani@gmail.com>
2019-07-31 18:34:43 +0530

    Tag helper basics

---
+++ WebAppTagHelperPrj/TagHelpers/AutoPriceTagHelper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAppTagHelperPrj.TagHelpers
+{
+    public class AutoPriceTagHelper : TagHelper
+    {
+        [HtmlAttributeNotBound]
+        public string Make { get; set; }
+        [HtmlAttributeName("model-name")]
+        public string Model { get; set; }
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+
+        }
+    }
+}
